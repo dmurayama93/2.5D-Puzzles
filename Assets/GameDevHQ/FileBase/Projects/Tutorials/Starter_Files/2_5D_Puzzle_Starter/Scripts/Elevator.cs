756bc7968b4dfe92bc083a363f8cf8cd189ede9a
dmurayama93 <dylan@gamedevhq.com>
2020-11-30 20:37:41 -1000

    Added coin requirement, and elevator function

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/Elevator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public Transform PointA;
+    public Transform PointB;
+    private Transform _curTarget;
+    private float _speed = 1.5f;
+    public void CallElevator()
+    {
+        //know the current position of the elevator
+        if (transform.position == PointA.position)
+        {
+            _curTarget = PointB;
+        }
+    }
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _curTarget.position, _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/ElevatorPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPanel : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _callButton;
+    private int _requiredCoins = 8;
+    private Elevator _elevator;
+
+    private void Start()
+    {
+        _elevator = GameObject.Find("Elevator").GetComponent<Elevator>();
+        if (_elevator == null)
+        {
+            Debug.LogError("The Elevator is Null");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E) && other.GetComponent<Player>().CoinCount() >= _requiredCoins)
+            {
+                _callButton.material.color = Color.green;
+                
+                _elevator.CallElevator();
+            }
+        }
+    }
+}
